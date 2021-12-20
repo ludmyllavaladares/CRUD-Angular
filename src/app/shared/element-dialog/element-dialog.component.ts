6feef981ef7fbcfc7535509757f17aadad88993f
valadaresludmylla@gmail.com <valadaresludmylla@gmail.com>
2021-12-20 16:14:33 -0300

    criação da tabela e modal para inserção do CRUD estático

---
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -0,0 +1,31 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PeriodicElement } from 'src/app/views/home/home.component';
+
+@Component({
+  selector: 'app-element-dialog',
+  templateUrl: './element-dialog.component.html',
+  styleUrls: ['./element-dialog.component.scss']
+})
+export class ElementDialogComponent implements OnInit {
+  element!: PeriodicElement;
+  isChange!: boolean;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: PeriodicElement,
+    public dialogRef: MatDialogRef<ElementDialogComponent>,
+  ) {}
+
+  ngOnInit(): void {
+    if(this.data.position != null) {
+      this.isChange = true;
+    }
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+
+}
